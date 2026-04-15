6f459371fd1286039711b15c4957dcb27ad84e2d
royalraja2354-del <royalraja2354@gmail.com>
2026-04-15 12:09:45 +0530

    Update ASSIGNMET_01.C

--- ASSIGNMET_01.C
+++ ASSIGNMET_01.C
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+int main() {
+    int n, sum = 0;
+    int avg;
+
+    printf("enter the value of n=");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("array");
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i <= n; i++) {
+        sum += arr[i];
+    }
+
+    avg = sum / n;
+
+    // printf("%d", avg);
+
+    int add;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > avg) {
+            add += arr[i];
+        }
+    }
+
+    printf("the add of greater number is %d", add);
+
+    return 0;
+}
